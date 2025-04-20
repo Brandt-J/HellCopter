11d4e236e2d1306920a64053f61898076bcf7103
Josef Brandt <josefbrandt@posteo.de>
2025-04-20 22:57:28 +0200

    Terrain3D Addon

--- Utils/FollowCam.gd
+++ Utils/FollowCam.gd
@@ -5,6 +5,7 @@ class_name FollowCamera
 var _node_to_follow: Node3D
 @export var camera_distance: float = 25.0
 @export var camera_height: float = 10.0
+@export var look_at_offset: float = 10.0
 @onready var _camera_3d = $Camera3D
 
 
@@ -14,5 +15,6 @@ func set_node_to_follow(node_to_follow: Node3D) -> void:
 
 func _process(_delta: float):
 	if is_instance_valid(_node_to_follow):
-		_camera_3d.global_position = _node_to_follow.global_position + Vector3(0.0, camera_height, camera_distance)
-	
+		var pos: Vector3 = _node_to_follow.to_global(Vector3(0, camera_height, camera_distance))
+		_camera_3d.global_position = pos
+		_camera_3d.look_at(_node_to_follow.global_position - _node_to_follow.global_basis.z*look_at_offset)
