2d43520856f8cfb663f1d028088246ce94aee5cb
Josef Brandt <josefbrandt@posteo.de>
2025-04-23 15:52:50 +0200

    Fix CameraBehavior

--- Utils/FollowCam.gd
+++ Utils/FollowCam.gd
@@ -11,10 +11,11 @@ var _node_to_follow: Node3D
 
 func set_node_to_follow(node_to_follow: Node3D) -> void:
 	_node_to_follow = node_to_follow
-
+	_camera_3d.global_position = _node_to_follow.to_global(Vector3(0, camera_height, camera_distance))
+	
 
 func _process(delta: float):
 	if is_instance_valid(_node_to_follow):
 		var pos: Vector3 = _node_to_follow.to_global(Vector3(0, camera_height, camera_distance))
-		_camera_3d.global_position = lerp(_camera_3d.global_position, pos, 0.95*delta)
+		_camera_3d.global_position = lerp(_camera_3d.global_position, pos, 0.98*delta)
 		_camera_3d.look_at(_node_to_follow.global_position - _node_to_follow.global_basis.z*look_at_offset)
